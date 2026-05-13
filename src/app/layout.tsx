f36a1e82531559a1f4762e1ef9a50fb53c9badd6
prem-patil2005 <patilprem.psp.2005@gmail.com>
2026-05-13 16:18:08 +0530

    global css added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import type { Metadata } from "next";
-import "tailwindcss";
+import "@/styles/globals.css";
 
 export const metadata: Metadata = {
   title: "SupplySetu",
